211f21f13d8aaa5e77c6598b18ae6048d9aaaea8
EduDalla <62569881+EduDalla@users.noreply.github.com>
2024-10-31 23:00:18 -0300

    Subindo correções do Desafio

--- src/Desafio.java
+++ src/Desafio.java
@@ -3,52 +3,52 @@ import java.util.Scanner;
 public class Desafio {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
+        String nome = "Eduardo";
+        String tipoConta = "Corrente";
+
         double saldo = 1200;
         int escolha = 0;
+
+        String menu = """
+                Operações
+                
+                1 - Consultar saldos"
+                2 - Receber valor
+                3 - Transferir valor
+                4 - Sair
+                """;
 
         while (escolha != 4) {
             System.out.println(" ");
 
-            System.out.println("Operações");
-
-            System.out.println(" ");
-            System.out.println("1 - Consultar saldos");
-            System.out.println("2 - Receber valor");
-            System.out.println("3 - Transferir valor");
-            System.out.println("4 - Sair");
-            System.out.println(" ");
+            System.out.println(menu);
 
             System.out.print("Digite a opção desejada: ");
             escolha = leitura.nextInt();
 
             if (escolha == 1) {
-                System.out.println("Seu saldo atual é R$" + saldo);
-            }
-            if (escolha == 2) {
+                System.out.println("Seu saldo atual é R$" + saldo + " e o tipo de conta é " + tipoConta);
+            } else if (escolha == 2) {
                 System.out.print("Informe o valor que está recebendo: ");
                 double recebendo = leitura.nextDouble();
                 saldo += recebendo;
                 System.out.println("Saldo atualizado para R$" + saldo);
-            }
-            if (escolha == 3) {
+            } else if (escolha == 3) {
                 System.out.print("Informe o valor que deseja transferir: ");
                 double transferencia = leitura.nextDouble();
 
                 if (transferencia > saldo) {
-                    System.out.print("Não há saldo suficiente para fazer esse transferência.");
-                    System.out.println(" ");
+                    System.out.println("Não há saldo suficiente para fazer esse transferência.");
                 } else {
                     saldo -= transferencia;
                     System.out.println("Saldo atualizado para R$" + saldo);
                 }
-            }
-            if (escolha > 4 || escolha < 0) {
+            } else if (escolha != 4) {
                 System.out.println("Opção inválida!");
-
             }
         }
 
-        System.out.println("Obrigado por confiar na EDU Bank! Volte sempre!");
+        System.out.println("Obrigado por confiar na EDU Bank, " + nome + "! Volte sempre!");
 
     }
 }
